04dae4ff518688ff5987fc2c17271acc19cce57f
Flor Campos Flores <u201601859@upc.edu.pe>
2019-12-01 09:46:54 -0500

    commit

---
+++ EjercicioWhile01/EjercicioWhile01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioWhile01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Pedir N enteros hasta que se ingrese CERO
+             * luego imprimir la suma de todos los dígitos.
+             * Ingreso:
+             *          234
+             *          768  
+             *          0
+             * Salida:
+             *          30
+             */
+            //int numero, suma, dig;
+            //do
+            //{
+            //    Console.Write("Ingrese número: ");
+            //    numero = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            //} while (numero != 0);
+
+            //Console.WriteLine("La suma de los dígitos es : ");
+            //Console.ReadKey();
+            int sum = 0;
+
+            while (true)
+            {
+                Console.Write("Ingrese número [0=FIN]: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (x == 0)
+                {
+                    break;
+                }
+                while (x >0)
+                {
+                    sum += x % 10;
+                    x /= 10;
+                }
+            }
+
+            Console.WriteLine("La suma de los dígitos es : " + sum);
+            Console.ReadKey();
+            // También se puede hacer con do - while
+        }
+    }
+}
---
+++ EjercicioWhile01/EjercicioWhile02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioWhile02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Pedir N valores hasta que se ingrese CERO
+             * luego imprimir la suma, el mayor, el menor, el promedio de todos los valores ingresados.
+             * Excepto el cero */
+            double sum = 0 , cuantos = 0, max =0, min =0;
+            
+            //double min = Double.MaxValue; // es el menor de todos los números reales10mim max
+            //double max = Double.MinValue;
+
+            while (true)
+            {
+                Console.Write("Ingrese valor [0=FIN]: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (x == 0)
+                {
+                    break;
+                }
+                ++cuantos;
+
+                if (cuantos ==1)
+                {
+                    max = min = x;
+                }
+
+                max = (x > max) ? x : max;
+                min = (x < min) ? x : min;
+                sum +=x;
+            }
+
+            if (cuantos > 0)
+            {
+                Console.WriteLine("La suma es : " + sum);
+                Console.WriteLine("El máx valor es :" + max);
+                Console.WriteLine("El min valor es :" + min);
+                Console.WriteLine("Promedio : " + (sum / cuantos));
+
+            }
+            else
+            {
+                Console.WriteLine("No ingresó valor válido");
+            }
+                                   
+            Console.ReadKey();
+            // También se puede hacer con do - while
+        }
+    }
+}
